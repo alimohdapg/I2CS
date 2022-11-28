9479aa0f57ee4d69f91ddb723bbbe7f59ab8dbf4
alimohdapg <alimohdapg@gmail.com>
2022-11-28 23:49:58 +0000

    Added logout statements before logging in

--- I2CS/urls.py
+++ I2CS/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('evaluation/', include('evaluation.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('accounts/',  include('django.contrib.auth.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
