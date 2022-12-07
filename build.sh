9decfe3baa02af5eab60e972f60a46e616109dd4
alimohdapg <alimohdapg@gmail.com>
2022-12-07 17:20:32 +0000

    commit

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -o errexit
 poetry install
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
+python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate
