95b2297c57e4bc3acb5b5c5378036e31fa6ec164
alimohdapg <alimohdapg@gmail.com>
2022-12-07 18:58:57 +0000

    commit

--- evaluation/models.py
+++ evaluation/models.py
@@ -52,6 +52,6 @@ methods_of_contact = (('phone', 'Phone'), ('email', 'Email'))
 
 class Evaluations(models.Model):
     details = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images')
     contact_method = models.CharField(max_length=10, choices=methods_of_contact)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
