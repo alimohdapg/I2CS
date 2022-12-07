76a8f0256df19e570c2d17658e3c4ea0078604e4
alimohdapg <alimohdapg@gmail.com>
2022-12-07 17:48:47 +0000

    commit

--- I2CS/urls.py
+++ I2CS/urls.py
@@ -28,7 +28,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
